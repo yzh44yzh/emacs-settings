d6c0dc64e697971f8d724195b32f9efbf5906580
Yura Zhloba <yzh44yzh@gmail.com>
2011-12-17 20:10:40 +0300

    add initial emacs settings

---
+++ init.el
@@ -0,0 +1,18 @@
+(setq inhibit-startup-message t)
+(global-linum-mode) ;; show line numbers
+
+(require 'color-theme)
+(eval-after-load "color-theme"
+  '(progn (color-theme-initialize)))
+(color-theme-calm-forest)
+
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
+  (when (fboundp 'tool-bar-mode)
+    (tool-bar-mode nil))
+  (when (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode nil)
+    (setq default-vertical-scroll-bar nil))
+  (blink-cursor-mode nil))
+
+
