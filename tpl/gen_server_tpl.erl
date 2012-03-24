80c1699be66989b1e86e7ef33d8da0a2dd88104a
Yura Zhloba <yzh44yzh@gmail.com>
2012-03-24 21:27:36 +0300

    update gen_server_tpl

--- tpl/gen_server_tpl.erl
+++ tpl/gen_server_tpl.erl
@@ -10,7 +10,7 @@
 %%% module API
 
 start_link() ->
-    gen_server:start_link(?MODULE, [], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 
 
@@ -18,23 +18,23 @@ start_link() ->
 %%% gen_server API
 
 init([]) ->
-	error_logger:info_msg("~p inited ~n", [?MODULE]),
-	{ok, []}.
+    error_logger:info_msg("~p inited ~n", [?MODULE]),
+    {ok, []}.
 
 
 handle_call(Request, _From, State) ->
-	error_logger:error_msg("unknown call ~p in ~p ~n", [Request, ?MODULE]),
-	{noreply, State}.
+    error_logger:error_msg("unknown call ~p in ~p ~n", [Request, ?MODULE]),
+    {noreply, State}.
 
 
 handle_cast(Request, State) ->
-	error_logger:error_msg("unknown cast ~p in ~p ~n", [Request, ?MODULE]),
-	{noreply, State}.
-	
+    error_logger:error_msg("unknown cast ~p in ~p ~n", [Request, ?MODULE]),
+    {noreply, State}.
+
 
 handle_info(Request, State) ->
-	error_logger:error_msg("unknown info ~p in ~n", [Request, ?MODULE]),
-	{noreply, State}.
+    error_logger:error_msg("unknown info ~p in ~n", [Request, ?MODULE]),
+    {noreply, State}.
 
 
 terminate(_Reason, _State) ->
