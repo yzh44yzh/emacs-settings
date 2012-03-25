16894b6d3ca9c235050e3927c72dd199ecef9fd2
Yura Zhloba <yzh44yzh@gmail.com>
2012-03-25 15:53:31 +0300

    improve erlang templates

--- tpl/gen_server_tpl.erl
+++ tpl/gen_server_tpl.erl
@@ -6,9 +6,12 @@
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
+-record(state, {}).
 
 %%% module API
 
+%% Starts the server
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
@@ -17,21 +20,46 @@ start_link() ->
 
 %%% gen_server API
 
+%% Initializes the server
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
 init([]) ->
     error_logger:info_msg("~p inited ~n", [?MODULE]),
-    {ok, []}.
+    {ok, #state{}}.
 
 
-handle_call(Request, _From, State) ->
-    error_logger:error_msg("unknown call ~p in ~p ~n", [Request, ?MODULE]),
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State};
+
+handle_call(Any, _From, State) ->
+    error_logger:error_msg("unknown call ~p in ~p ~n", [Any, ?MODULE]),
     {noreply, State}.
 
 
-handle_cast(Request, State) ->
-    error_logger:error_msg("unknown cast ~p in ~p ~n", [Request, ?MODULE]),
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+handle_cast(_Request, State) ->
+    {noreply, State};
+
+handle_cast(Any, State) ->
+    error_logger:error_msg("unknown cast ~p in ~p ~n", [Any, ?MODULE]),
     {noreply, State}.
 
 
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
 handle_info(Request, State) ->
     error_logger:error_msg("unknown info ~p in ~n", [Request, ?MODULE]),
     {noreply, State}.
@@ -43,3 +71,4 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVersion, State, _Extra) ->
     {ok, State}.	
+
--- tpl/module_tpl.erl
+++ tpl/module_tpl.erl
@@ -4,3 +4,4 @@
 -export([]).
 
 %%% module API
+
