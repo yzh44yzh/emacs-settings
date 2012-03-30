5b6c256fd2a759ee8bcf80d203bed53c5932ad52
Yura Zhloba <yzh44yzh@gmail.com>
2012-03-30 08:42:03 +0300

    remove vim style cursor moving, not use it

--- hotkeys.el
+++ hotkeys.el
@@ -1,12 +1,6 @@
 ;; 57.4.6 Rebinding Keys in Your Init File
 ;; http://www.gnu.org/s/libtool/manual/emacs/Init-Rebinding.html
 
-;; hjkl -- vim style cursor moving
-(global-set-key (kbd "M-h") 'backward-char) ;; default is mark-paragraph
-(global-set-key (kbd "M-j") 'next-line)     ;; default is indent-new-comment-line
-(global-set-key (kbd "M-k") 'previous-line) ;; default is kill-sentence
-(global-set-key (kbd "M-l") 'forward-char)  ;; default is downcase-word
-
 ;; switch buffers
 (global-set-key [M-left] 'previous-buffer) ;; default is backword-word
 (global-set-key [M-right] 'next-buffer)    ;; default is forward-word
