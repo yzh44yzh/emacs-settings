a7a3eda48f5cada65afb95930aca351d13b4926e
Yura Zhloba <yzh44yzh@gmail.com>
2012-11-28 17:28:03 +0300

    enable ido

--- init.el
+++ init.el
@@ -51,14 +51,6 @@
       (lambda ()
         (list "All")))
 
-;; iswitchb settings
-(iswitchb-mode 1)
-(defun iswitchb-local-keys ()
-  (mapc (lambda (K) 
-          (let* ((key (car K)) (fun (cdr K)))
-            (define-key iswitchb-mode-map (edmacro-parse-keys key) fun)))
-        '(("<right>" . iswitchb-next-match)
-          ("<left>"  . iswitchb-prev-match)
-          ("<up>"    . ignore             )
-          ("<down>"  . ignore             ))))
-(add-hook 'iswitchb-define-mode-map-hook 'iswitchb-local-keys)
+;; ido
+(require 'ido)
+(ido-mode t)
