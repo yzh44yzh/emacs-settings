6042e54f7225dd43f0f0c29a0c03b16c27666cd6
Yura Zhloba <yzh44yzh@gmail.com>
2013-03-23 21:06:21 +0300

    merge with linux settings

--- init.el
+++ init.el
@@ -1,51 +1,55 @@
 (setq inhibit-startup-message t)
 (global-linum-mode) ;; show line numbers
 (show-paren-mode 1)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
 (setq make-backup-files nil) 
 (setq auto-save-default nil)
-(menu-bar-mode -1)
-(set-face-attribute 'default nil :font "Monaco-15")
-;; (tabbar-mode 1)
-(iswitchb-mode 1)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(blink-cursor-mode 0)
 (setq mac-command-modifier 'meta)
 
-;;(require 'color-theme)
-;; (eval-after-load "color-theme"
-;;   '(progn (color-theme-initialize)))
-
-;; (color-theme-billw) ;; color theme for console mode
-
-(when window-system
-  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
-  (when (fboundp 'tool-bar-mode)
-    (tool-bar-mode 0))
-  (when (fboundp 'scroll-bar-mode)
-    (scroll-bar-mode nil)
-    (setq default-vertical-scroll-bar nil))
-  (blink-cursor-mode nil)
-  ;;(color-theme-calm-forest)
-)
-
-(load-file "~/.emacs.d/hotkeys.el")
+(set-face-attribute 'default nil :font "Monaco-15")
+(load-theme 'wheatgrass t)
 
 ;; erlang mode
 (add-to-list 'load-path "/usr/local/lib/erlang/lib/tools-2.6.7/emacs")
 
 (add-to-list 'auto-mode-alist '("\\.erl?$" . erlang-mode))
 (add-to-list 'auto-mode-alist '("\\.hrl?$" . erlang-mode))
+(add-to-list 'auto-mode-alist '("\\.yaws?$" . html-mode))
 
 (setq erlang-root-dir "/usr/lib/erlang")
 (add-to-list 'exec-path "/usr/lib/erlang/bin")
 (setq erlang-man-root-dir "/usr/lib/erlang/man")
 
 (require 'erlang-start)
-;;(require 'erlang-flymake)
+;;(require 'erlang-flymake)  ;; doesn't work correctly with include_lib
 
-(load-file "~/.emacs.d/erl-utils.el")
 
 ;; haskell
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-to-list 'load-path "~/dev/haskell-mode/")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'haskell-font-lock-symbols t)
+(set 'haskell-font-lock-symbols t)
+(put 'downcase-region 'disabled nil)
 
-;; (load-file "~/.emacs.d/server.el")
+;; ido
+(require 'ido)
+(ido-mode t)
 
+(load-file "~/.emacs.d/erl-utils.el")
+(load-file "~/.emacs.d/hotkeys.el")
+
+
+;; Objective C
+(defun my-objc-mode-hook ()
+  (setq c-indent-level 4)
+  (setq c-basic-offset 4))
+
+(add-hook 'objc-mode-hook 'my-objc-mode-hook)
 
