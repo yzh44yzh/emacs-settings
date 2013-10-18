f980c320d98af6a34c108afb232b1ff32288799c
Yura Zhloba <yzh44yzh@gmail.com>
2013-10-18 21:29:24 +0300

    refactor erl-utils, more clean code

--- hotkeys.el
+++ hotkeys.el
@@ -1,19 +1,19 @@
 ;; 57.4.6 Rebinding Keys in Your Init File
 ;; http://www.gnu.org/s/libtool/manual/emacs/Init-Rebinding.html
 
+
 ;; more useful hotkeys:
 (global-set-key [C-return] 'complete-symbol)    ;; no default
 (global-set-key (kbd "C-M-s") 'grep)            ;; default is isearch-forward-regexp
 (global-set-key (kbd "C-M-r") 'query-replace)   ;; default is isearch-backward-regexp
 (global-set-key (kbd "C-M-j") 'delete-indentation) ;; default is indent-new-comment line (also M-j) 
 
+
 ;; shadow bad hotkeys:
 ;; I never want to put emacs to background
 (global-set-key (kbd "C-z") 'undo)            ;; default is to put emacs to background
 ;; very ofter press it when I want switch buffer with C-x b
 (global-set-key (kbd "C-x C-b") 'ido-switch-buffer) ;; default is list-buffers, 
-
-
 
 
 ;; copy region if it exists 
@@ -28,7 +28,6 @@
     (save-excursion
       (clipboard-kill-ring-save (line-beginning-position) (+ 1 (line-end-position)))
       (kill-ring-save (line-beginning-position) (+ 1 (line-end-position))))))
-
 (global-set-key (kbd "M-w") 'my-copy) ;; default is kill-ring-save
 
 
@@ -44,17 +43,21 @@
     (progn
       (clipboard-kill-ring-save (line-beginning-position) (+ 1 (line-end-position)))
       (kill-region (line-beginning-position) (+ 1 (line-end-position))))))
-
 (global-set-key (kbd "C-w") 'my-cut) ;; default is kill-region
+
 
 ;; duplicate line
 (defun duplicate-line()
   (interactive)
-  (kill-region (line-beginning-position) (+ 1 (line-end-position)))
+  (setq kill-ring nil)
+  (beginning-of-line)
+  (kill-line)
   (yank)
+  (newline)
   (yank)
-  (previous-line))
+  (setq kill-ring nil))
 (global-set-key (kbd "C-M-d") 'duplicate-line) 
+
 
 (defun my-new-line()
   (interactive)
@@ -62,11 +65,14 @@
   (newline-and-indent))
 (global-set-key (kbd "C-j") 'my-new-line)
 
+
 (defun my-grep()
   (interactive)
   (setq arg (format "grep -nH -e %s *" (word-at-point)))
   (grep arg))
 (global-set-key (kbd "M-s s") 'my-grep)
+
+
 
 ; @author Nikita Danilov http://www.cofault.com/2011/12/cue-key.html
 ; 
