c428114f19936993fcc77fa604e79130d394fb2e
Yura Zhloba <yzh44yzh@gmail.com>
2016-07-27 09:25:01 +0300

    ocaml support

--- init.el
+++ init.el
@@ -123,6 +123,23 @@
 
 
 ;; -------------
+;; Ocaml
+;; -------------
+
+(require 'cl)
+(require 'tuareg)
+
+(setq auto-mode-alist
+      (append '(("\\.ml[ily]?$" . tuareg-mode)
+                ("\\.topml$" . tuareg-mode))
+              auto-mode-alist))
+
+(add-hook 'tuareg-mode-hook 'merlin-mode)
+(setq merlin-use-auto-complete-mode t)
+(setq merlin-error-after-save nil)
+
+
+;; -------------
 ;; org-mode
 ;; -------------
 
